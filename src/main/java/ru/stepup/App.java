500002a7747ea81d057d6530888fac8582674f5f
PaulSPuntusov <pps_ru@mail.ru>
2024-07-03 22:16:03 +0300

    Из таргета App.class джавой не запускается. (А если просто Build) то App.class джавой запускается. Слава, подскажи, пож-та, почему

---
+++ src/main/java/ru/stepup/App.java
@@ -0,0 +1,14 @@
+package ru.stepup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+
+    public static void main(String[] args )
+    {
+        System.out.println( "Hello WWWorld!" );
+    }
+}
